e28822f4f337781241807cb99acc63ee917bdc43
luis <luisrolandocahuanaleon@gmail.com>
2023-03-04 10:21:25 +0100

    compoonente automatico, una belleza!!!!!

--- 01-BASES/src/app/app.module.ts
+++ 01-BASES/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroeComponent
+    HeroeComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule

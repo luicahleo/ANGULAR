73d48bb4f5ea48c3cf9a7ec3e78f9bb3437e8f20
luis <luisrolandocahuanaleon@gmail.com>
2023-05-06 09:11:42 +0200

    01-basic-types

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+// import './topics/01-basic-types.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+ Hola mundo
+`
+
+console.log("hola");
---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,16 @@
+ const name = 'Luis';
+
+let hppoints:number | string = 95;
+
+const isAlive: boolean = true;
+
+
+hppoints = 45;
+hppoints = '45';
+
+ 
+console.log({
+    name, hppoints,isAlive
+});
+
+ export{};
